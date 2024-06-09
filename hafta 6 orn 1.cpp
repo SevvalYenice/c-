81132ef11a530872d8522189e10ccaa99db98f45
Şevval <sevvalync06@gmail.com>
2024-06-09 21:07:32 +0300

    Add files via upload

---
+++ hafta 6 orn 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello word"<<endl;
+	cout<<"hello word"<<endl;
+	int a=20;
+	int b;
+	b=15;
+	cout<<"20+15="<<a+b<<"olur."<<endl;
+	a=a+b;
+	cout<<a<<endl;
+	return 0;
+}
